0fa1e5c870e2385da97a31d4c76537cddee7020c
Dependability <Robloxplayer307@gmail.com>
2022-11-22 12:18:28 -0600

    Added css and decimal functionality, along with keyboard accessibility

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ let inputMode = false;
 let number1 = null;
 let number2 = null;
 let operation = null;
+let decimal = true;
 const numbers = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operate');
 
@@ -41,7 +42,16 @@ numbers.forEach((digit)=>{
     digit.addEventListener('click', (e)=>{
         if (!inputMode) {
             display.textContent = '';
+            decimal = true;
         }
+            if (digit.textContent == '.') {
+                if (!decimal) {
+                    return
+                }
+                decimal = false;
+                
+                
+            } 
             display.textContent += digit.textContent;
             inputMode = true;
     });
@@ -50,12 +60,32 @@ numbers.forEach((digit)=>{
 
 const compute = document.querySelector('.compute');
 const clear = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
 
 
 function resetColors(){
-    operators.forEach(operator => operator.style.backgroundColor = 'unset')
+    operators.forEach(operator => operator.classList.remove('selected') );
 }
 
+function deleteFunction () {
+    if (display.textContent == '') {
+        return
+    }
+
+    if (!inputMode) {
+        display.textContent = '0';
+        operation = null;
+        resetColors();
+        inputMode = false;
+        number1 = null;
+        number2 = null;
+    }   
+    if (display.textContent.charAt(display.textContent.length - 1) == '.') {
+        decimal = true;
+    } 
+    display.textContent = display.textContent.slice(0, -1);
+}
+deleteButton.addEventListener('click',deleteFunction )
 clear.addEventListener('click', ()=>{
     display.textContent = '0';
     operation = null;
@@ -75,20 +105,60 @@ compute.addEventListener('click',(e)=>{
 
 })
 
-operators.forEach((operator)=> {
-    operator.addEventListener('click', (e)=>{
-        if (number1 == null) {
-            number1 = +display.textContent;
-        } else if (number1 != null) {
-            console.log(number1)
-            number2 = +display.textContent
-            display.textContent = operate(operation, number1, number2)
-            number1 = +display.textContent
-        }
+function operatorFunction(e) {
+    if (number1 == null) {
+        number1 = +display.textContent;
+    } else if (number1 != null) {
+        console.log(number1)
+        number2 = +display.textContent
+        display.textContent = operate(operation, number1, number2)
+        number1 = +display.textContent
+    }
 
-        resetColors()
-        operation = operator.textContent;
-        operator.style.backgroundColor = 'grey'
-        inputMode = false;
-    });
+    resetColors()
+    operation = e.target.textContent;
+    e.target.classList.add('selected');
+    inputMode = false;
+}
+
+operators.forEach((operator)=> {
+    operator.addEventListener('click', operatorFunction);
 });
+
+const keys = [
+    "Backspace",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "*",
+    ".",
+    "+",
+    "-",
+    "/",
+    '=',
+    "Enter"
+];
+
+const operatorKeys = []
+
+const clickEvent = new Event("click");
+document.addEventListener('keydown', (e)=> {
+    if (keys.includes(e.key)) {
+        e.preventDefault()
+        if (e.key == "Enter") {
+            document.querySelector(`[key="="]`).dispatchEvent(clickEvent)
+            return
+        }
+        
+        document.querySelector(`[key="${e.key}"]`).dispatchEvent(clickEvent)
+        return;
+    }
+    
+})
